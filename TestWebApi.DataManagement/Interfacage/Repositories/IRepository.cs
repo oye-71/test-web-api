361a44f3e92fa8667df8c489daca772a600294af
Etienne DUMONTET <etienne.dumontet@coexya.eu>
2022-04-12 14:35:43 +0200

    Documentation IRepository

--- TestWebApi.DataManagement/Interfacage/Repositories/IRepository.cs
+++ TestWebApi.DataManagement/Interfacage/Repositories/IRepository.cs
@@ -10,13 +10,52 @@ namespace TestWebApi.DataManagement.Interfacage.Repositories
 {
     public interface IRepository<TEntity> where TEntity : IEntity
     {
+        /// <summary>
+        /// Récupère une entité en fonction de son id.
+        /// </summary>
+        /// <returns></returns>
         public Task<TEntity> GetById(Guid id);
+
+        /// <summary>
+        /// Récupère toutes les entités d'une table.
+        /// </summary>
+        /// <returns></returns>
         public Task<IEnumerable<TEntity>> GetAll();
+
+        /// <summary>
+        /// Ajoute une nouvelle entité en base.
+        /// </summary>
+        /// <returns></returns>
         public Task Add(TEntity entity);
+
+        /// <summary>
+        /// Ajoute plusieurs nouvelles entités en base.
+        /// </summary>
+        /// <returns></returns>
         public Task AddRange(IEnumerable<TEntity> entities);
+
+        /// <summary>
+        /// Met a jour une entité déjà existante en base.
+        /// </summary>
+        /// <returns></returns>
         public Task Update(TEntity entity);
+
+        /// <summary>
+        /// Vérifie si l'entité à ajouter existe, si c'est le cas la met à jour, sinon l'ajoute.
+        /// </summary>
+        /// <returns></returns>
         public Task AddOrUpdate(TEntity entity);
+
+        /// <summary>
+        /// Supprime une entité en base.
+        /// </summary>
+        /// <returns></returns>
         public Task Delete(TEntity entity);
+
+        /// <summary>
+        /// Supprime plusieurs entités en base.
+        /// </summary>
+        /// <returns></returns>
         public Task DeleteRange(IEnumerable<TEntity> entities);
     }
 }
